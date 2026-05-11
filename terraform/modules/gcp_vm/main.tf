cddccd6eb32929c63f2e5d39a016f4eb2a5394f0
MartaPenina <penina.marta100605@gmail.com>
2026-05-11 21:13:30 +0300

    fix: startup script CRLF fix - SSH port 9922 works automatically

--- terraform/modules/gcp_vm/main.tf
+++ terraform/modules/gcp_vm/main.tf
@@ -1,6 +1,5 @@
 resource "google_compute_instance" "vm" {
   for_each = var.config.general.cloud == "gcp" ? var.config.vms : {}
-
   name         = each.key
   machine_type = var.config.sizes[each.value.size].gcp
   zone         = var.config.locations[var.config.general.location].gcp.zones[each.value.zone]
@@ -23,18 +22,7 @@ resource "google_compute_instance" "vm" {
   }
 
   metadata = {
-    ssh-keys = "${var.config.general.ops_user}:${var.ssh_public_key}"
+    ssh-keys       = "${var.config.general.ops_user}:${var.ssh_public_key}"
+    startup-script = "#!/bin/bash\ncloud-init status --wait\nsystemctl disable --now ssh.socket\necho 'Port ${var.config.general.ssh_port}' > /etc/ssh/sshd_config.d/custom-port.conf\nsystemctl enable ssh.service\nsystemctl restart ssh.service\n"
   }
-
-  metadata_startup_script = <<-EOT
-    #!/bin/bash
-    if [ -f /etc/ssh/sshd_config.d/custom-port.conf ]; then
-      exit 0
-    fi
-    cloud-init status --wait
-    systemctl disable --now ssh.socket
-    echo "Port ${var.config.general.ssh_port}" > /etc/ssh/sshd_config.d/custom-port.conf
-    systemctl enable ssh.service
-    systemctl restart ssh.service
-  EOT
 }
